8a09c5539c6732837f25d789fde93c1938fef118
Nitesh Chowdhary <thenitesh0112@gmail.com>
2023-03-26 08:39:41 +0530

    add rainbow and black bgc changer

--- script.js
+++ script.js
@@ -1,36 +1,47 @@
 const grid = document.querySelector('.grid')
-// const black = document.querySelector('black')
-// const rainbow = document.querySelector('rainbow')
-
-
-let btn = 'black'
-
-function rainbow() {
-
-    const randomColor = randomColorgenerator()
-    btn = randomColor
-    console.log(btn)
-}
-
-function randomColorgenerator() {
-    let r = Math.floor(Math.random() * 255)
-    let g = Math.floor(Math.random() * 255)
-    let b = Math.floor(Math.random() * 255)
-    return `rgb(${r}, ${g}, ${b})`
-}
-function black() {
-    btn = 'black'
-    console.log(btn)
-}
-
-let colorButton = btn
+const rainbowBtn = document.querySelector('#rainbow-btn')
+const blackBtn = document.querySelector('#black-btn')
+const myColor = document.getElementById("myColor");
+const eraserBtn = document.getElementById("eraser-btn");
+const clearBtn = document.getElementById("clear-btn");
 
 for (let i = 0; i < 256; i++) {
     const div = document.createElement('div')
     div.classList.add('cell')
     div.addEventListener('mouseover', (e) => {
-        e.target.style.backgroundColor = btn
+        e.target.style.backgroundColor = myColor.value
     })
     grid.appendChild(div)
 }
 
+// clearBtn.addEventListener('click', () => {
+//     grid.addEventListener('click', (e) => {
+//         e.target.style.backgroundColor = 'white'
+//     })
+// })
+
+eraserBtn.addEventListener('click', () => {
+    grid.addEventListener('click', (e) => {
+        e.target.style.backgroundColor = 'white'
+    })
+})
+
+rainbowBtn.addEventListener('click', () => {
+    grid.addEventListener('mouseover', (e) => {
+        e.target.style.backgroundColor = randomColor()
+    })
+})
+
+blackBtn.addEventListener('click', () => {
+    grid.addEventListener('mouseover', (e) => {
+        e.target.style.backgroundColor = 'black'
+    })
+})
+
+function randomColor() {
+    let r = Math.floor(Math.random() * 256)
+    let g = Math.floor(Math.random() * 256)
+    let b = Math.floor(Math.random() * 256)
+
+    return `rgb(${r},${g},${b})`
+}
